0a74b57358eed72affa5fd5e06170d08f8f873f1
Gage Coprivnicar <super15gage@gmail.com>
2016-07-25 10:42:35 -0600

    fixed bugs; edited

--- main.py
+++ main.py
@@ -17,32 +17,44 @@ print "wiimote found"
 # set buttons to report when pressed
 wm.rpt_mode = cwiid.RPT_BTN
 
+def rumble():
+    wm.rumble = True
+    time.sleep(.1)
+    wm.rumble = False
+
+def change_lights():
+    light_number = wm.state['led']
+    led_state = b.get_light(light_number, 'on')
+    if led_state == True:
+        b.set_light(light_number, 'on', False)
+    if led_state == False:
+        b.set_light(light_number, 'on', True)
+
+def led_increase():
+    led_state = wm.state['led']
+    if led_state >= 16:
+        wm.led = 0
+    else:
+        wm.led = led_state + 1
+
+def check_leds():
+    if wm.state['led'] >= 16:
+        wm.led = 15
+    if wm.state['led'] <= 0:
+        wm.led = 0
+
 while True:
-    def rumble_when_pressed():
-        wm.rumble = True
-        time.sleep(.1)
-        wm.rumble = False
-
-    def change_lights():
-        light_number = wm.state['led']
-        led_state = b.get_light(light_number, 'on')
-        if led_state == True:
-            b.set_light(light_number, 'on', True)
-        if led_state == False:
-            b.set_light(light_number, 'on', False)
-        rumble_when_pressed()
-
-    def led_increase():
-        led_state = wm.state['led']
-        if led_state >= 16:
-            wm.led = 0
-        else:
-            wm.led = led_state + 1
 
     if (wm.state['buttons'] & cwiid.BTN_UP):
-        light_on()
+        wm.led = wm.state['led'] + 1
+        rumble()
+        check_leds()
+
     if (wm.state['buttons'] & cwiid.BTN_DOWN):
-        light_off()
+        wm.led = wm.state['led'] - 1
+        rumble()
+        check_leds()
+
     if (wm.state['buttons'] & cwiid.BTN_A):
         change_lights()
 
