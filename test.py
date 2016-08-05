599d3e533dbdc55b8f87ef5a00807a118ce1ab76
Gage Coprivnicar <super15gage@gmail.com>
2016-08-05 13:39:13 -0600

    commit

---
+++ test.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+=======
+import cwiid
+import time
+
+print "press 1 + 2 now"
+try:
+    # attempt to connect wii remote
+    wm = cwiid.Wiimote()
+except RuntimeError:
+    print "failed to find wiimote"
+print "wiimote found"
+
+wm.led = 1
+
+while True:
+    wm.led = wm.state['led'] + 1
+    time.sleep(1)
+    wm.led = wm.state['led'] + 1
+    time.sleep(1)
+    cwiid.Wiimote()
+    wm.led = wm.state['led'] + 1
+    time.sleep(1)
+>>>>>>> parent of fe7498a... finding a solution
