e2551267eb9165927fc7f2a16844d6323016b820
Gage Coprivnicar <super15gage@gmail.com>
2016-08-05 15:48:26 -0600

    commit

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
 import cwiid
 import time
 
-print "press 1 + 2 now"
-try:
-    # attempt to connect wii remote
-    wm = cwiid.Wiimote()
-except RuntimeError:
-    print "failed to find wiimote"
-print "wiimote found"
-
 wm.led = 15
 
-dict = {'state' : True}
+dict = {'state' : False}
 
 while True:
 
