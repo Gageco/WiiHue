fe1cb5d66b51200078c6e17aed3a8f6bf9eaa5ae
Gage Coprivnicar <super15gage@gmail.com>
2016-08-05 15:51:13 -0600

    commit

--- test.py
+++ test.py
@@ -1,29 +1,15 @@
 import cwiid
 import time
 
+print "press 1 + 2 now"
+try:
+    # attempt to connect wii remote
+    wm = cwiid.Wiimote()
+except RuntimeError:
+    print "failed to find wiimote"
+print "wiimote found"
 
-dict = {'state' : False}
+wm.led = 15
 
-while True:
-
-
-
-
-    if dict['state'] == False:
-        while dict['state'] == False:
-            print "1 + 2 now"
-            wm = cwiid.WiiMote()
-            print "connecetion successful"
-            wm.rumble = True
-            time.sleep(.5)
-            dict['state'] = True
-            wm.rumble = False
-
-        if dict['state'] == True:
-            while dict['state'] == True:
-                if (wm.state['buttons'] & cwiid.BTN_A):
-                    wm.close()
-                    print "WiiMote Closed"
-                    dict['state'] = False
-
-    time.sleep(.5)
+wm.close()
+print "connection closed"
