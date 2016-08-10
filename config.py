d4e7bbceb248f1151078fade7530d1630d25ae09
Gage Coprivnicar <super15gage@gmail.com>
2016-08-10 10:19:19 -0600

    commit

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 #HUE IP ADDRESS
-'192.168.0.105'
-#Get the ip from https://www.meethue.com/api/nupnp
+192.168.0.105
 #Room configuration SETUP: 'kitchen', [1,2,3]
 #                           TITLE       LIGHT NUMs
 #Put rooms between START and END only support for 2 rooms thus far
